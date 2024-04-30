79653010fadcf525263e6247b5f5d7d8b70948be
yu <kd1338110@st.kobedenshi.ac.jp>
2024-04-30 13:41:34 +0900

    Hello,errorの提出課題

---
+++ public/error.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+    <html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>エラーPHP</title> <!-- タイトルも変更する -->
+    </head>
+
+    <body>
+        <?php
+        echo <p>Hello,PHP</p>; // 文字列をシングルクォーテーションで囲まなかった  
+        ?>
+        <p>0J04010倉光悠晴</p>
+    </body>
+
+    </html>
